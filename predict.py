99c1b7bed5e8628d0aa8ba776be986325e59d0c7
tongxinoptica <90898694+tongxinoptica@users.noreply.github.com>
2022-04-01 09:34:46 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -8,9 +8,9 @@ import torch.nn as nn
 
 Tensor = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor()])
 RDR_model = RDR_model()
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # 定义训练的设备
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  
 RDR_model.to(device)
-print("模型加载完成")
+print("model ready")
 
 Image_path = 'D:/picture/robustness/translation/1.5f-80.jpg'
 label_path = 'D:/picture/robustness/gt.jpg'
@@ -19,7 +19,7 @@ img = Tensor(img)
 img = torch.reshape(img, (1, 1, 256, 256))
 label = Image.open(label_path)
 label = Tensor(label)
-label = torch.reshape(label, (1, 1, 256, 256))  # 增加batch维度
+label = torch.reshape(label, (1, 1, 256, 256)) 
 RDR_model.load_state_dict(torch.load('./parameter/mse_gr36_1.5f'))
 b = torch.ones(256, 256).to(device)
 
@@ -42,7 +42,7 @@ with torch.no_grad():
     print(ssim)
     #print(ssim1)
     to_pil = transforms.ToPILImage()
-    output_pil = to_pil(output[0])  # 删去batch维度
+    output_pil = to_pil(output[0])  
     plt.imshow(output_pil, cmap='gray')
     plt.axis('off')
     plt.savefig('D:/out1.5f-80.jpg', bbox_inches='tight', pad_inches=0)
